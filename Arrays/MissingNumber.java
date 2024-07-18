0967317f233c659cf6407d96255e21faab5687e1
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-07-18 16:26:39 +0530

    Better Approach for Missing number in an array.

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -17,11 +17,27 @@ public class MissingNumber {
     }
 
     // Better Approach 
+
+    public static int missingNumber(int []a, int N) {
+
+        int hash[] = new int[N + 1];
+
+        for (int i = 0; i < N - 1; i++)
+            hash[a[i]]++;
+
+        for (int i = 1; i <= N; i++) {
+            if (hash[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) { 
         int N = 5;
         int a[] = {1, 2, 4, 5};
 
-        int ans = findMissing(a, N);
+        int ans = missingNumber(a, N);
         System.out.println("The missing number is: " + ans);
     }
 }
