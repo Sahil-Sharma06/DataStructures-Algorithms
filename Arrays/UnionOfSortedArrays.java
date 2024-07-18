69c448b0d5bf65d495007301a477d13b727f5b0b
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-07-18 15:08:18 +0530

    Optimal approach

--- Arrays/UnionOfSortedArrays.java
+++ Arrays/UnionOfSortedArrays.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class UnionOfSortedArrays {
+    // Bruteforce Approach 
     static ArrayList<Integer> unionOfSortedArrays(int[] arr1, int[] arr2){
         HashSet<Integer> set = new HashSet<>();
         ArrayList<Integer> Union = new ArrayList<>();
@@ -13,6 +14,46 @@ public class UnionOfSortedArrays {
 
         for(int x: set){
             Union.add(x);
+        }
+
+        return Union;
+    }
+
+    // Optimal Approach 
+
+    static ArrayList<Integer> unionOptimal(int[] arr1, int[] arr2){
+        int n = arr1.length;
+        int m = arr2.length;
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> Union = new ArrayList<Integer>();
+        while(i < n && j < m){
+            if(arr1[i] <= arr2[j]){
+                if(Union.size() == 0 || Union.get(Union.size()-1) != arr1[i]){
+                    Union.add(arr1[i]);
+                }
+                i++;
+          }else{
+            if(Union.size() == 0 || Union.get(Union.size()-1) != arr2[j]){
+                Union.add(arr2[j]);
+            }
+            j++;
+          }
+        }
+
+        while(i < n){
+            if(Union.get(Union.size()-1) != arr1[i]){
+                Union.add(arr1[i]);
+            }
+            i++;
+        }
+
+        
+        while(j < m){
+            if(Union.get(Union.size()-1) != arr2[j]){
+                Union.add(arr2[j]);
+            }
+            j++;
         }
 
         return Union;
@@ -32,7 +73,7 @@ public class UnionOfSortedArrays {
             arr2[i] = sc.nextInt();
         }
 
-        ArrayList<Integer> Union = unionOfSortedArrays(arr1, arr2);
+        ArrayList<Integer> Union = unionOptimal(arr1, arr2);
         for(int x: Union){
             System.out.print(x+" ");
         }
