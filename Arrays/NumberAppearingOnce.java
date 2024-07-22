fd3781b63727093996e123a7c798fcd632ee81dc
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-07-22 16:11:15 +0530

    Number appearing Once in the array

---
+++ Arrays/NumberAppearingOnce.java
@@ -0,0 +1,25 @@
+public class NumberAppearingOnce {
+
+    static int numberAppearingOnce(int[] arr,int N){
+        for(int i = 0 ; i < N ; i++){
+            int num = arr[i];
+            int count = 0;
+            for(int j = 0 ; j < N ; j++){
+                if(arr[j] == num){
+                    count++;
+                }
+            }
+            if(count == 1){
+                return num;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int n = 5;
+        int ans = numberAppearingOnce(arr, n);
+        System.out.println("The single element is: " + ans);
+
+    }
+}
