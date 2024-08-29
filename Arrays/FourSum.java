28cd37e7efe05652135845053fda4ea35ec4efff
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-08-29 23:37:42 +0530

    4 sum optimal approach

--- Arrays/FourSum.java
+++ Arrays/FourSum.java
@@ -62,6 +62,43 @@ public static List<List<Integer>> fourSumBetter(int[] nums, int target) {
     return ans;
 }
 
+// Optimal Approach 
+
+public static List<List<Integer>> fourSumOptimal(int[] nums, int target) {
+    int n = nums.length;
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+
+    for (int i = 0; i < n; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int j = i + 1; j < n; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l) {
+                long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    temp.add(nums[l]);
+                    ans.add(temp);
+                    k++;
+                    l--;
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
+                } else if (sum < target) k++;
+                else l--;
+            }
+        }
+    }
+    return ans;
+}
+
 public static void main(String[] args) {
     int[] nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
     int target = 9;
