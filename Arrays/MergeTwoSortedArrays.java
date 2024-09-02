4afa14ac1471966f4be106f42e1d8821605a5bb5
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-02 23:38:30 +0530

    Optimal approach 1

--- Arrays/MergeTwoSortedArrays.java
+++ Arrays/MergeTwoSortedArrays.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class MergeTwoSortedArrays {
 
     // Bruteforce Approach
@@ -33,11 +34,32 @@ public class MergeTwoSortedArrays {
         }
     }
 
+    // Optimal Approach 1 
+    public static void mergeOptimal1(long[] arr1, long[] arr2, int n, int m){
+        int left = n-1;
+        int right = 0;
+
+        while(left >= 0 && right < m){
+            if(arr1[left] > arr2[right]){
+                long temp = arr1[left];
+                arr1[left] = arr2[right];
+                arr2[right] = temp;
+                left--;
+                right++;
+            }else{
+                break;
+            }
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
     public static void main(String[] args) {
         long[] arr1 = { 1, 4, 8, 10 };
         long[] arr2 = { 2, 3, 9 };
         int n = 4, m = 3;
-        merge(arr1, arr2, n, m);
+        mergeOptimal1(arr1, arr2, n, m);
         System.out.println("The merged arrays are:");
         System.out.print("arr1[] = ");
         for (int i = 0; i < n; i++) {
