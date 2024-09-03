1548a408416e3d3088f88dd0cf3226428c342c78
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-03 17:13:14 +0530

    Merge overlapping optimal

--- Arrays/MergeOverlappingSubIntervals.java
+++ Arrays/MergeOverlappingSubIntervals.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.*;
 public class MergeOverlappingSubIntervals {
 
+    // Bruteforce Approach 
     public static ArrayList<ArrayList<Integer>> mergeOverlappingBrute(int[][] arr){
         int n = arr.length;
         Arrays.sort(arr, new Comparator<int[]>(){
@@ -33,9 +34,32 @@ public class MergeOverlappingSubIntervals {
 
         return ans;
     }
+
+    // Optimal Approach 
+    public static ArrayList<ArrayList<Integer>> mergerOverlappingIntervalsOptimal(int[][] arr){
+        int n = arr.length;
+
+        Arrays.sort(arr,new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return a[0]-b[0];
+            }
+        });
+
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i++){
+            if(ans.isEmpty() || arr[i][0] > ans.get(ans.size()-1).get(1)){
+                ans.add(new ArrayList<>(Arrays.asList(arr[i][0],arr[i][1])));
+            }else{
+                ans.get(ans.size()-1).set(1,Math.max(ans.get(ans.size()-1).get(1), arr[i][1]));
+            }
+        }
+
+        return ans;
+    }
     public static void main(String[] args) {
          int[][] arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
-        ArrayList<ArrayList<Integer>> ans = mergeOverlappingBrute(arr);
+        ArrayList<ArrayList<Integer>> ans = mergerOverlappingIntervalsOptimal(arr);
         System.out.print("The merged intervals are: \n");
         for (List<Integer> it : ans) {
             System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
