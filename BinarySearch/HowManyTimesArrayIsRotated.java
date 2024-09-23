9dacac96278e13f38c5aea9b14aa25b03419136a
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-23 14:17:53 +0530

    Find K number of rotations in a sorted array - Brute and Optimal

---
+++ BinarySearch/HowManyTimesArrayIsRotated.java
@@ -0,0 +1,59 @@
+public class HowManyTimesArrayIsRotated {
+
+    // BruteForce Approach 
+    public static int findKRotation(int[] arr) {
+        int n = arr.length;
+        int ans = Integer.MAX_VALUE, index = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < ans) {
+                ans = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    
+    // Optimal Approach 
+
+    public static int findKRotationOptimal(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
+            }
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+
+                low = mid + 1;
+            } else { 
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findKRotation(arr);
+        System.out.println("The array is rotated " + ans + " times.");
+    }
+}
