f5f6153d07f5e859aa7e3867bb9122abe709df7a
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-10-18 16:31:18 +0530

    Swap 2 numbers and Ith bit is set or not

--- BitManipulation/src/IthBitSetOrNot.java
+++ BitManipulation/src/IthBitSetOrNot.java
@@ -5,6 +5,7 @@ public class IthBitSetOrNot {
         int n = sc.nextInt();
         int i = sc.nextInt();
         if((n & (1 << i)) != 0)
+//            Alternate by using right shift operator if((n >> i) & 1 == 0) -> not set else set
             System.out.println("The bit at i'th position is set");
         else
             System.out.println("The bit is not set");
