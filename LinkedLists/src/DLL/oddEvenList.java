73cc3b7e155ffa3124cc35aa582f06da25f75e8f
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-11-01 16:27:16 +0530

    Odd Even Linked List optimal code

--- LinkedLists/src/DLL/oddEvenList.java
+++ LinkedLists/src/DLL/oddEvenList.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static DLL.insertionInA2DLL.head;
+
 public class oddEvenList {
+//    Bruteforce approach
     static Node oddEvenLinkedList(Node head){
+        if(head == null || head.next == null)
+            return head;
         Node temp = head;
         List<Integer> arr = new ArrayList<>();
         while(temp != null && temp.next != null){
@@ -31,6 +36,26 @@ public class oddEvenList {
         }
         return head;
     }
+
+//    Optimal Approach
+
+    static Node oddEvenOptimal(Node head){
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return  head;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -53,7 +78,7 @@ public class oddEvenList {
         System.out.println("Original Linked List:");
         introDLL.printLL(head);
 
-        head = oddEvenLinkedList(head);
+        head = oddEvenOptimal(head);
 
         System.out.println("Transformed Linked List (Odd-Even arrangement):");
         introDLL.printLL(head);
